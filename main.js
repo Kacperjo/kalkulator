a98f2d5531c753f4be6d1c808b71a734398878c3
kapeć <jodelkok@gmail.com>
2025-08-01 19:44:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let num1 = prompt("podaj pierwszą liczbe")
-let num2 = prompt("podaj druga liczbe")
+let num1 = parseFloat(prompt("podaj pierwszą liczbe"))
+let num2 = praseFloat(prompt("podaj druga liczbe"))
 let sum1 = num1 + num2
-document.getElementById("wynik") = sum1
+document.getElementById("wynik").innerHTML = sum1
